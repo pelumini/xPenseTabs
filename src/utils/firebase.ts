dbb2aa9d89fc9b0a493d52a11345e0eab4ae8636
pelumini <pelumini@gmail.com>
2025-02-07 18:53:47 +0000

    firebase config variables moved to env

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,12 +4,12 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBEOJ30fEEMGj8ulMmLV7AIwD_FfABECHY",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: "expensetracker-20785.firebaseapp.com",
   projectId: "expensetracker-20785",
   storageBucket: "expensetracker-20785.firebasestorage.app",
-  messagingSenderId: "22779155647",
-  appId: "1:22779155647:web:5aa1ca333cfc9f5322e20a",
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_D,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
